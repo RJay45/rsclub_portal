351326aa21ac6d942709b815ee2ba39c2962a506
Jason Griffith <jason@rjay45.co.uk>
2015-10-15 18:37:37 +0100

    WIP

--- rsapi/api.py
+++ rsapi/api.py
@@ -1,6 +1,11 @@
 __author__ = 'jason'
 
+from .models import ApiKey
+from datetime import datetime
 
 class Api:
     def __init__(self):
-        print("echo")
+        now = datetime.now()
+        keys = ApiKey.objects.all().filter(startTime__lte=now).filter(endTime__gt=now)
+        print("Found the following keys:")
+        print(keys)
--- rsapi/views.py
+++ rsapi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rsapi.api import Api
+
 
 # Create your views here.
+def index(request):
+    j = Api()
+    return HttpResponse("Testing 123...")
