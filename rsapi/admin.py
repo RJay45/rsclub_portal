b8e19a7d6d810ce2e7ce6ecf7143698228863514
Jason Griffith <jason@rjay45.co.uk>
2015-10-15 23:50:15 +0100

    Create admin listing for API keys and add formatting for expired keys.

--- rsapi/admin.py
+++ rsapi/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import ApiKey
 
+
+class ApiKeyAdmin(admin.ModelAdmin):
+    list_display = ('formatted_user_id', 'formatted_auth_key', 'formatted_start_time', 'formatted_end_time',
+                    'formatted_url')
+    search_fields = ('userId', 'authKey',  'startTime', 'endTime', 'url')
+    ordering = ['-endTime']
+
+
 # Register your models here.
-admin.site.register(ApiKey)
+admin.site.register(ApiKey, ApiKeyAdmin)
--- rsapi/models.py
+++ rsapi/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
+from django.utils.html import format_html
 
 
 # Create your models here.
@@ -8,3 +11,44 @@ class ApiKey(models.Model):
     authKey = models.CharField(max_length=255)
     startTime = models.DateTimeField()
     endTime = models.DateTimeField()
+
+    def is_valid(self):
+        now = timezone.make_aware(datetime.now(), timezone.get_current_timezone())
+        return self.startTime >= now or self.endTime < now
+
+    def formatted_url(self):
+        if self.is_valid():
+            return self.url
+        else:
+            return ApiKey._add_strike(self.url)
+
+    def formatted_user_id(self):
+        if self.is_valid():
+            return self.userId
+        else:
+            return ApiKey._add_strike(self.userId)
+
+    def formatted_auth_key(self):
+        if self.is_valid():
+            return self.authKey
+        else:
+            return ApiKey._add_strike(self.authKey)
+
+    def formatted_start_time(self):
+        if self.is_valid():
+            return datetime.strftime(self.startTime, "%c")
+        else:
+            return ApiKey._add_strike(datetime.strftime(self.startTime, "%c"))
+
+    def formatted_end_time(self):
+        if self.is_valid():
+            return datetime.strftime(self.endTime, "%c")
+        else:
+            return ApiKey._add_strike(datetime.strftime(self.endTime, "%c"))
+
+    @staticmethod
+    def _add_strike(value):
+        return format_html("<span style=\"text-decoration: line-through;\">" + value + "</span>")
+
+    def __unicode__(self):
+        return u'%s:%s' % (self.userId, self.authKey)
