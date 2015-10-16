ef9f2fc96cc8777b4f4e58fcdf93fa40365a6e91
Jason Griffith <jason@rjay45.co.uk>
2015-10-17 00:38:24 +0100

    Correct admin APIKey column names; Fix date range check in is_valid(); Add _api_call method

--- rsapi/api.py
+++ rsapi/api.py
@@ -1,11 +1,57 @@
 __author__ = 'jason'
 
 from .models import ApiKey
-from datetime import datetime
+from urllib import request, error
+import json
+
 
 class Api:
     def __init__(self):
-        now = datetime.now()
-        keys = ApiKey.objects.all().filter(startTime__lte=now).filter(endTime__gt=now)
-        print("Found the following keys:")
-        print(keys)
+        self.key = None
+        self._check_key()
+        print(self.key)
+        data = self._api_call('domains/available.json', {'domain-name':'google', 'tlds':'com'})
+        print(data)
+
+    def _check_key(self):
+        if self.key is None or not self.key.is_valid():
+            for key in ApiKey.get_valid():
+                self.key = key
+                if self._check_connection():
+                    break
+                else:
+                    self.key = None
+
+        if self.key is None:
+            print("No valid key found!")
+            return False
+
+        return True
+
+    def _check_connection(self):
+        return self.key is not None \
+            and self._api_call('domains/available.json', {'domain-name':'google', 'tlds':'com'}) is not None
+
+    def _api_call(self, command, kv_pairs=None):
+        if self.key is None:
+            raise Exception("No API key available. Unable to make API call.")
+
+        if command is None or not isinstance(command, str):
+            raise Exception("An API command must be specified")
+
+        #Create base API call with authentication data
+        call = self.key.url + command + "?auth-userid=" + self.key.userId + "&api-key=" + self.key.authKey
+
+        #Add additional parameters
+        if kv_pairs is not None and isinstance(kv_pairs, dict):
+            for item in kv_pairs.items():
+                call += "&" + item[0] + "=" + item[1]
+
+        print(call)
+
+        try:
+            apicall = request.Request(call)
+            return json.loads(request.urlopen(apicall).read().decode('utf-8'))
+        except error.HTTPError as e:
+            print("Error - HTTP code " + str(e.code) + " response: " + e.reason)
+            return None
--- rsapi/models.py
+++ rsapi/models.py
@@ -13,42 +13,56 @@ class ApiKey(models.Model):
     endTime = models.DateTimeField()
 
     def is_valid(self):
+        """Returns True if this key is valid (now is between the start and end times)"""
         now = timezone.make_aware(datetime.now(), timezone.get_current_timezone())
-        return self.startTime >= now or self.endTime < now
+        return self.startTime <= now < self.endTime
+
+    @staticmethod
+    def get_valid():
+        """Get all ApiKeys that are valid."""
+        now = timezone.make_aware(datetime.now(), timezone.get_current_timezone())
+        keys = ApiKey.objects.all().filter(startTime__lte=now).filter(endTime__gt=now)
+        #print(keys.query)
+        return keys
 
     def formatted_url(self):
         if self.is_valid():
             return self.url
         else:
             return ApiKey._add_strike(self.url)
+    formatted_url.short_description = "URL"
 
     def formatted_user_id(self):
         if self.is_valid():
             return self.userId
         else:
             return ApiKey._add_strike(self.userId)
+    formatted_user_id.short_description = "User ID"
 
     def formatted_auth_key(self):
         if self.is_valid():
             return self.authKey
         else:
             return ApiKey._add_strike(self.authKey)
+    formatted_auth_key.short_description = "Key"
 
     def formatted_start_time(self):
         if self.is_valid():
             return datetime.strftime(self.startTime, "%c")
         else:
             return ApiKey._add_strike(datetime.strftime(self.startTime, "%c"))
+    formatted_start_time.short_description = "Start Time"
 
     def formatted_end_time(self):
         if self.is_valid():
             return datetime.strftime(self.endTime, "%c")
         else:
             return ApiKey._add_strike(datetime.strftime(self.endTime, "%c"))
+    formatted_end_time.short_description = "End Time"
 
     @staticmethod
     def _add_strike(value):
         return format_html("<span style=\"text-decoration: line-through;\">" + value + "</span>")
 
-    def __unicode__(self):
-        return u'%s:%s' % (self.userId, self.authKey)
+    def __str__(self):
+        return '%s:%s' % (self.userId, self.authKey)
